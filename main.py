9ba1364cb26e5d4f0a514d92725f13c92674f0b2
Joaquin H <hernandez.joaquin346@gmail.com>
2025-04-19 17:08:29 -0300

    raw get and post tasks mehods

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from DataBase import JsonDataBase
+from pydantic import BaseModel
 
 app = FastAPI()
+db = JsonDataBase("tasks.json")
+
+class Task(BaseModel):
+    description:str
+    status:str 
+    def __str__(self):
+        return f"description: {self.description}, status: {self.description}"
 
 @app.get("/")
 def root():
     return {"message": "Hello world"}
 
+@app.post("/tasks")
+def post_task(task:Task):
+    db.add(task.description)
+    return {"response": f"new task '{task.description}' added", "status": 200}
+
+@app.get("/tasks")
+def get_tasks():
+    tasks = db.getItems()
+    return tasks 
+
+@app.get("/tasks/{id}")
+def get_task(id:int):
+    task = db.getItem(id)
+    return {"data":task}
